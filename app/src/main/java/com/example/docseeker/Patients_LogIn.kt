e531450e7eeca66cbf5388d9ccec6a2396c94ce5
KendallRcs <kendallramiro@gmail.com>
2023-04-26 16:12:35 -0500

    layout designed

--- app/src/main/java/com/example/docseeker/Patients_LogIn.kt
+++ app/src/main/java/com/example/docseeker/Patients_LogIn.kt
@@ -2,10 +2,13 @@ package com.example.docseeker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 
-class MainActivity : AppCompatActivity() {
+class Patients_LogIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_patients_log_in)
     }
 }
