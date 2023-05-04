dcf45e2a58f0764701b827bcf427ec9f27358dd4
KendallRcs <kendallramiro@gmail.com>
2023-05-04 01:23:41 -0500

    added activity new details and layout

--- app/src/main/java/com/example/docseeker/NewsAdapter.kt
+++ app/src/main/java/com/example/docseeker/NewsAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.docseeker
 
 import Beans.News
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
@@ -13,6 +15,7 @@ class NewsAdapter(var news: Array<News>) : RecyclerView.Adapter<NewsAdapter.News
     class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val titleView: TextView = itemView.findViewById(R.id.newTitle)
         val imageView: ImageView = itemView.findViewById(R.id.newImage)
+        val cardView: CardView = itemView.findViewById(R.id.newCard)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.new_card, parent, false)
@@ -25,6 +28,14 @@ class NewsAdapter(var news: Array<News>) : RecyclerView.Adapter<NewsAdapter.News
         Glide.with(holder.itemView)
             .load(new.imageUrl)
             .into(holder.imageView)
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(holder.itemView.context, NewDetails::class.java)
+            intent.putExtra("titleNew", new.title)
+            intent.putExtra("imageUrlNew", new.imageUrl)
+            intent.putExtra("descriptionNew", new.description)
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
     fun updateNews(newNewsList: Array<News>) {
