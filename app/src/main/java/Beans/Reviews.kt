b2f43624742512a4173435bf1b4caf32394414b7
Joyce <josabavi123@gmail.com>
2023-05-28 10:45:08 -0500

    add Reviews

--- app/src/main/java/Beans/Reviews.kt
+++ app/src/main/java/Beans/Reviews.kt
@@ -5,16 +5,19 @@ class Reviews {
     var description: String
     var doctor_id: Int
     var patient_id: Int
+    var rating: Int
 
     constructor(
         id: Int,
         description: String,
         doctor_id: Int,
-        patient_id:Int
+        patient_id:Int,
+        rating: Int
     ){
         this.id= id
         this.description= description
         this.doctor_id=doctor_id
         this.patient_id=patient_id
+        this.rating=rating
     }
 }
