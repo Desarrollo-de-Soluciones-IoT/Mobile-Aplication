1b8b3b81b78cc2c6be3a2d58c6fe1a6192ed4fb8
KendallRcs <kendallramiro@gmail.com>
2023-06-25 11:54:49 -0500

    baseUrl changed

--- app/src/main/java/com/example/docseeker/BaseUrl.kt
+++ app/src/main/java/com/example/docseeker/BaseUrl.kt
@@ -1,7 +1,7 @@
 package com.example.docseeker
 
 object BaseUrl {
-    //const val base_url = "https://spring-docseeker-dockseeker-be.azuremicroservices.io/api/v1/"
-    const val base_url = "http://192.168.1.4:8080/api/v1/"
+    const val base_url = "https://spring-docseeker-dockseeker-be.azuremicroservices.io/api/v1/"
+    //const val base_url = "http://192.168.1.4:8080/api/v1/"
 
 }
